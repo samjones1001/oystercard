26a40d90e40e8eb3f7cd0e045b01224d651e3072
Basile Koko <basile_koko@hotmail.com>
2017-01-09 14:47:24 +0000

    topping_up oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,12 @@
 class Oystercard
-    DEFAULT_BALANCE = 0
-    
+
     attr_reader :balance
-    
-    def initialize(balance = DEFAULT_BALANCE)
-        @balance = balance
+
+    def initialize
+        @balance = 0
+    end
+
+    def top_up(amount)
+      @balance += amount
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,12 +2,19 @@ require 'oystercard'
 
 describe Oystercard do
     it { is_expected.to respond_to(:balance) }
-    
+    it {is_expected.to respond_to(:top_up).with(1).argument}
+
     subject { described_class.new }
-    
+
     describe '#balance' do
         it 'a new card should return 0 balance' do
             expect(subject.balance).to eq 0
         end
+    end
+
+    describe '#top_up' do
+       it 'add amount of balance' do
+        expect{subject.top_up 10}.to change {subject.balance}.by 10
+      end
     end
 end
