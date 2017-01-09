fe3e99e0ef4793f3e9c78a7838535bc5aaadd171
Sam Jones <sam.jones494@googlemail.com>
2017-01-09 22:14:03 +0000

    Adds error message when card balance exceeded

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,21 @@
 class Oystercard
 
 	attr_reader :balance
+	CARD_LIMIT = 90
 
 	def initialize
 		@balance = 0
 	end
 
 	def top_up(amount)
+		raise ("Card balance limit of #{CARD_LIMIT} exceeded") if max_balance(amount)
 		@balance += amount
+	end
+
+	private
+
+	def max_balance(amount)
+		@balance + amount > CARD_LIMIT
 	end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,9 +12,12 @@ describe Oystercard do
 
 	describe '#top_up' do
 
-		it 'tops up the card balance' do
-			
+		it 'tops up the card balance' do			
 			expect{card.top_up(1)}.to change{card.balance}.by(1)
+		end
+
+		it "raises an error when exceeding card limit" do
+			expect{card.top_up(Oystercard::CARD_LIMIT + 1)}.to raise_error("Card balance limit of #{Oystercard::CARD_LIMIT} exceeded")
 		end
 
 	end
