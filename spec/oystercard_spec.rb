d1a06c4de691a8f4d324bea0281a7330d19da262
Benjamin Carson <benjohncarson@gmail.com>
2017-01-09 14:16:52 +0000

    created default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do
-
+    it { is_expected.to respond_to(:balance) }
+    
+    subject { described_class.new }
+    
+    describe '#balance' do
+        it 'a new card should return 0 balance' do
+            expect(subject.balance).to eq 0
+        end
+    end
 end
