1b59059fd7a1635f2267532d5a5ce5af86142af0
Basile Koko <basile_koko@hotmail.com>
2017-01-10 17:46:26 +0000

    handling journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,15 +1,19 @@
 class Journey
     attr_reader :trip
-    
+
     def initialize
         @trip = {}
     end
-    
+
     def start_journey(entry_station)
         trip[:entry_station] = entry_station
     end
-    
+
     def finish_journey(exit_station)
         trip[:exit_station] = exit_station
+    end
+
+    def complete_journey
+       trip.size == 2
     end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,28 +2,51 @@ require 'journey'
 
 describe Journey do
     subject {described_class.new}
-    
+
     let(:entry_station) { double :station }
     let(:exit_station)  { double :station }
-    
+
     before do
         allow(entry_station).to receive(:name).and_return("Bank")
         allow(entry_station).to receive(:zone).and_return(1)
         allow(exit_station).to receive(:name).and_return("Poplar")
         allow(exit_station).to receive(:zone).and_return(2)
     end
-    
+
     describe '#start_journey' do
         it 'sets an entry_station' do
             subject.start_journey(entry_station)
             expect(subject.trip[:entry_station]).to eq entry_station
         end
     end
-    
+
     describe '#finish_journey' do
         it 'sets an exit_station' do
             subject.finish_journey(exit_station)
             expect(subject.trip[:exit_station]).to eq exit_station
         end
     end
+
+    describe '#complete_journey' do
+      context  'check for complete journey' do
+        before do
+          subject.start_journey(:entry_station)
+          subject.finish_journey(:exit_station)
+      end
+      it 'returns true for complete journey' do
+        expect(subject.complete_journey).to eq true
+      end
+    end
+       context 'tap out without tapping in' do
+         before do
+           subject.finish_journey(:exit_station)
+         end
+         it 'return incompleted journey false' do
+             expect(subject.complete_journey).to eq false
+         end
+       end
+    end
+    describe '#fare' do
+    end
+
 end
